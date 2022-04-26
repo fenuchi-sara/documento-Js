97d2afd555bb417cf620ee2e63db12423699e404
fenuchi-sara <sarafenuchi@gmail.com>
2022-04-26 16:41:21 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,23 @@
+function clickHere(){
+    document.querySelector(".evento").innerHTML="Clique"
+}
+
+function clickMe(){
+    document.querySelector(".evento").innerHTML="Passou"
+}
+
+function goout(){
+    document.querySelector(".evento").innerHTML="Ja Passo"
+}
+
+function carConteudo(){
+    document.getElementById("conteudo").innerHTML = "Integer ac pulvinar risus, vel lobortis sapien. Maecenas ut iaculis velit. Fusce diam augue, sodales et purus sed, pharetra rutrum erat.";
+}
+
+function mouseOver(){
+    document.getElementById("mouseAlt").innerHTML = "Retire o mouse";
+}
+
+function mouseOut(){
+    document.getElementById("mouseAlt").innerHTML = "Passe o mouse";
+}
